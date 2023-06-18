8ba934af842e2d2ed1aacf5315e047005af8d0bc
xxyeon129 <xxyeon129@gmail.com>
2023-06-18 18:01:41 +0900

    [Fix] 주석처리해서 오류나는 컴포넌트 Router 주석처리

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,10 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Login from "../pages/Login";
-import Signup from "../pages/Signup";
-import ChatRoom from "../pages/ChatRoom";
-import UserList from "../pages/UserList";
-import MyPage from "../pages/MyPage";
-import Main from "../pages/Main";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
+import ChatRoom from '../pages/ChatRoom';
+import UserList from '../pages/UserList';
+import MyPage from '../pages/MyPage';
+import Main from '../pages/Main';
+import SignupPage from '../pages/SignupPage';
 
 const Router = () => {
   return (
@@ -12,10 +13,11 @@ const Router = () => {
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/ChatRoom/:id' element={<ChatRoom />} />
-          {<Route path='/userslist' element={<UserList />} />}
-          <Route path='/MyPage/:id' element={<MyPage />} />
+          {/* <Route path='/signup' element={<Signup />} /> */}
+          <Route path='/signup' element={<SignupPage />} />
+          {/* <Route path='/ChatRoom/:id' element={<ChatRoom />} />
+          <Route path='/userslist' element={<UserList />} />
+          <Route path='/MyPage/:id' element={<MyPage />} /> */}
           <Route path='/Main' element={<Main />} />
         </Routes>
       </BrowserRouter>
