4042a0b851844d2c29b7ba740ee6f5c05250f737
xxyeon129 <xxyeon129@gmail.com>
2023-06-22 00:57:23 +0900

    feat: 재사용 모달 컴포넌트, 친구 추가 모달 구현

--- src/components/Friends.jsx
+++ src/components/Friends.jsx
@@ -1,13 +1,24 @@
-import React from "react";
-import StFriends from "../style/_friends";
-import List from "../style/_list";
-import ProfileImg from "./element/ProfileImg";
+import React, { useState } from 'react';
+import StFriends from '../style/_friends';
+import List from '../style/_list';
+import ProfileImg from './element/ProfileImg';
+import Portal from '../utils/portal';
+import Modal from './element/Modal';
+import useInput from '../hooks/useInput';
 
 const Friends = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const { inputData, inputChangeHandler } = useInput();
+
+  const addFriendHandler = () => {};
+
   return (
     <StFriends>
       <nav>
-        <button className="addFriends">친구 추가하기</button>
+        <button className='addFriends' onClick={() => setIsModalOpen(true)}>
+          친구 추가하기
+        </button>
       </nav>
       <section>
         <h2>온라인 친구</h2>
@@ -18,6 +29,20 @@ const Friends = () => {
           </li>
         </List>
       </section>
+      <Portal>
+        <Modal
+          isModalOpen={isModalOpen}
+          setIsModalOpen={setIsModalOpen}
+          inputData={inputData}
+          inputChangeHandler={inputChangeHandler}
+          buttonClickHandler={addFriendHandler}
+          title='친구 추가'
+          description='사용자 email을 사용하여 친구를 추가할 수 있어요.'
+          label='사용자 email'
+          buttonContent='친구 추가하기'
+          id='addFriend'
+        />
+      </Portal>
     </StFriends>
   );
 };
