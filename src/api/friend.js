84be65eb612bf9322b6e3f0c98afee8f9502702d
xxyeon129 <xxyeon129@gmail.com>
2023-06-21 23:50:26 +0900

    refactor: axios instance 적용

--- src/api/friend.js
+++ src/api/friend.js
@@ -1,18 +1,10 @@
-import axios from 'axios';
-import { BASE_URL } from '../shared/constants';
-import { getAccessToken } from './getToken';
+import instance from './axios';
 
 const RESOURCE = '/users/friend';
 
 export const getFriendList = async () => {
   try {
-    const accesstoken = await getAccessToken();
-    const response = await axios.get(`${BASE_URL}${RESOURCE}`, {
-      headers: {
-        Authorization: accesstoken,
-      },
-    });
-
+    const response = await instance.get(RESOURCE);
     return response.data;
   } catch (error) {
     console.error('getFriendList Axios Error', error);
